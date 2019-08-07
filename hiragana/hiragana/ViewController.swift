b345a6d80de3c16f950f3a3a0b003cfa05222deb
erikamatsubara <erika.matsubara0518@gmail.com>
2019-08-07 15:27:47 +0900

    XCodeプロジェクトを作成

---
+++ hiragana/hiragana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hiragana
+//
+//  Created by 松原えりか on 2019/08/07.
+//  Copyright © 2019 松原えりか. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
